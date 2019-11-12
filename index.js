4350367f0ec81a6a26bdaae0ca3b5b22b5ac992e
AJ Guiles <ashiguiles@gmail.com>
2019-11-12 02:24:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 
-function scooter(year, color, model) {
+function Scooter(year, color, model) {
   this.year = year,
   this.color = color,
   this.model = model
 }
 
-function driver(name, age, experience) {
+function Driver(name, age, experience) {
   this.name = name,
   this.age = age,
   this.experience = experience
 }
 
-function pickUpLocation(address, city) {
+function pPickUpLocation(address, city) {
   this.address = address,
   this.city = city,
 
