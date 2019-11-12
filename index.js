d1ca2fdb5f974dc74abf88ca929234ae1b9d7dd6
AJ Guiles <ashiguiles@gmail.com>
2019-11-12 02:20:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function scooter(year, color, model) {
   this.color = color,
   this.model = model
 }
+
+function driver(year, color, model) {
+  this.year = year,
+  this.color = color,
+  this.model = model
+}
